5180c0fac4add58978600cc4fc6af4d53cd2d0ee
NicollasPetrin <134434413+NicollasPetrin@users.noreply.github.com>
2025-04-14 10:41:34 -0300

    Add files via upload

--- src/js/script.js
+++ src/js/script.js
@@ -1,55 +1,40 @@
 // Pedindo os dados ao usuario
 
-var vinho = prompt("Digite o nome do vinho");
+function cadastrarVinho() {
+    var vinho = prompt("Digite o nome do vinho");
+
     if (vinho == ""){
         alert("O nome do vinho deve ser inserido !");
         var vinho = prompt("Digite o nome do vinho");
-    }
-var tipo = prompt("Digite o tipo do vinho");
+    };
+
+    var tipo = prompt("Digite o tipo do vinho");
+
     if (tipo == ""){
         alert("O tipo do vinho deve ser inserido !");
         var tipo = prompt("Digite o tipo do vinho");
-    }
-var ano = prompt("Digite o ano do vinho");
+    };
+
+    var ano = prompt("Digite o ano do vinho");
+
     if (ano == ""){
         alert("O ano do vinho deve ser inserido !");
         var ano = parseInt(prompt("Digite o ano do vinho"));
-    }
-var estoque = prompt("Digite a quantidade de vinho");
+    };
+
+    var estoque = prompt("Digite a quantidade de vinho");
+
     if (estoque == ""){
         alert("O estoque do vinho deve ser inserido !");
         var vinho = parseInt(prompt("Digite a quantidade de vinho"));
-    }
+    };
 
-// mostrar que o cadastro esta realizado e cadastro no console
+    // mostrar que o cadastro esta realizado e cadastro no console
 
-alert ("Cadastro realizado com sucesso");
-alert ("A seguir, veja os detalhes no console !");
-console.log ("nome do vinho : " + vinho);
-console.log ("tipo do vinho : " + tipo);
-
-
-if(estoque < 5){
-    console.log ("estoque do vinho : " + estoque + "     ESTOQUE BAIXO");
-    if(ano >= 2020 ){
-        console.log("ano do vinho : " + ano + "      Vinho Jovem");
-    }
-    if(ano >= 2015 && ano <= 2019){
-        console.log("ano do vinho : " + ano + "     Vinho Amadurecido");
-    }
-    if(ano < 2015){
-        console.log("ano do vinho : " + ano + "     Vinho Antigo");
-    }
-    
-}else {
+    alert ("Cadastro realizado com sucesso");
+    alert ("A seguir, veja os detalhes no console !");
+    console.log ("nome do vinho : " + vinho);
+    console.log ("tipo do vinho : " + tipo);
+    console.log ("ano do vinho : " + ano);
     console.log ("estoque do vinho : " + estoque);
-    if(ano >= 2020 ){
-        console.log("ano do vinho : " + ano + "      Vinho Jovem");
-    }
-    if(ano >= 2015 && ano <= 2019){
-        console.log("ano do vinho : " + ano + "     Vinho Amadurecido");
-    }
-    if(ano < 2015){
-        console.log("ano do vinho : " + ano + "     Vinho Antigo");
-    }
 }
